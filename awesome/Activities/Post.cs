eab34de9104510f3d6543ce7df46ea038c4142f3
Keita <koron0902@gmail.com>
2019-09-09 22:20:00 +0900

    コメントの投稿画面を作成した

---
+++ awesome/Activities/Post.cs
@@ -0,0 +1,45 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+using Android.Support.Design.Widget;
+using Android.Support.V7.App;
+
+namespace awesome.Activities {
+	[Activity(Label = "Post", Theme = "@style/AppTheme.NoActionBar")]
+	public class Post : AppCompatActivity {
+		protected override void OnCreate(Bundle savedInstanceState) {
+			base.OnCreate(savedInstanceState);
+
+			// Create your application here
+			SetContentView(Resource.Layout.activity_post);
+
+			Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar_post);
+			toolbar.Title = "";
+			SetSupportActionBar(toolbar);
+		}
+		public override bool OnCreateOptionsMenu(IMenu menu) {
+			MenuInflater.Inflate(Resource.Menu.menu_post, menu);
+			return true;
+		}
+
+		public override bool OnOptionsItemSelected(IMenuItem item) {
+			int id = item.ItemId;
+			if(id == Resource.Id.menu_posting) {
+				Finish();
+				return true;
+			}
+
+			return base.OnOptionsItemSelected(item);
+		}
+	}
+}
--- awesome/MainActivity.cs
+++ awesome/MainActivity.cs
@@ -19,8 +19,9 @@ namespace awesome {
 			Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
 			SetSupportActionBar(toolbar);
 
-			FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
-			fab.Click += FabOnClick;
+			FindViewById<FloatingActionButton>(Resource.Id.fab).Click += (sender, e)=>{
+				StartActivity(new Android.Content.Intent(ApplicationContext, typeof(Activities.Post)));
+			};
 		}
 
 		public override bool OnCreateOptionsMenu(IMenu menu) {
@@ -37,11 +38,6 @@ namespace awesome {
 			return base.OnOptionsItemSelected(item);
 		}
 
-		private void FabOnClick(object sender, EventArgs eventArgs) {
-			View view = (View)sender;
-			Snackbar.Make(view, "Replace with your own action", Snackbar.LengthLong)
-					.SetAction("Action", (Android.Views.View.IOnClickListener)null).Show();
-		}
 		public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults) {
 			Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
