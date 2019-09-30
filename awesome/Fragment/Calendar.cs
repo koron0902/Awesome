689b7d768dc7af57323f28cc1f7e6deeb7037dab
Keita <koron0902@gmail.com>
2019-09-30 17:19:25 +0900

    Re-replace TAB into SPACE

--- awesome/Fragment/Calendar.cs
+++ awesome/Fragment/Calendar.cs
@@ -14,44 +14,44 @@ using Android.Widget;
 using Android.Support.V7.App;
 
 namespace awesome.Fragment {
-	public class Calendar : DialogFragment {
-		Java.Util.Calendar calendar_;
-		DatePickerDialog datePicker_;
-		Context context_;
+  public class Calendar : DialogFragment {
+    Java.Util.Calendar calendar_;
+    DatePickerDialog datePicker_;
+    Context context_;
 
-		public Action<DatePickerDialog.DateSetEventArgs> onDataSelectChanged;
+    public Action<DatePickerDialog.DateSetEventArgs> onDataSelectChanged;
 
-		public Calendar(Context _context) {
-			context_ = _context;
-		}
+    public Calendar(Context _context) {
+      context_ = _context;
+    }
 
-		protected Calendar(IntPtr javaReference, JniHandleOwnership transfer) : base(javaReference, transfer) {
-		}
+    protected Calendar(IntPtr javaReference, JniHandleOwnership transfer) : base(javaReference, transfer) {
+    }
 
-		public override void OnCreate(Bundle savedInstanceState) {
-			base.OnCreate(savedInstanceState);
-		}
+    public override void OnCreate(Bundle savedInstanceState) {
+      base.OnCreate(savedInstanceState);
+    }
 
 
-		public override Dialog OnCreateDialog(Bundle savedInstanceState) {
-			base.OnCreateDialog(savedInstanceState);
-			calendar_ = Java.Util.Calendar.Instance;
-			datePicker_ = new DatePickerDialog(context_);
-			datePicker_.DateSet += (sender, e) => {
-				var str = e.Date.ToLocalTime().ToString("YYYY-MM-DD HH:mm:ss");
-				onDataSelectChanged(e);
-			};
-			calendar_.Get(Java.Util.CalendarField.Year);
-			calendar_.Get(Java.Util.CalendarField.Month);
-			calendar_.Get(Java.Util.CalendarField.DayOfMonth);
-			return datePicker_;
-		}
+    public override Dialog OnCreateDialog(Bundle savedInstanceState) {
+      base.OnCreateDialog(savedInstanceState);
+      calendar_ = Java.Util.Calendar.Instance;
+      datePicker_ = new DatePickerDialog(context_);
+      datePicker_.DateSet += (sender, e) => {
+        var str = e.Date.ToLocalTime().ToString("YYYY-MM-DD HH:mm:ss");
+        onDataSelectChanged(e);
+      };
+      calendar_.Get(Java.Util.CalendarField.Year);
+      calendar_.Get(Java.Util.CalendarField.Month);
+      calendar_.Get(Java.Util.CalendarField.DayOfMonth);
+      return datePicker_;
+    }
 
-		public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-			// Use this to return your custom view for this Fragment
-			// return inflater.Inflate(Resource.Layout.YourFragment, container, false);
+    public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+      // Use this to return your custom view for this Fragment
+      // return inflater.Inflate(Resource.Layout.YourFragment, container, false);
 
-			return base.OnCreateView(inflater, container, savedInstanceState);
-		}
-	}
+      return base.OnCreateView(inflater, container, savedInstanceState);
+    }
+  }
 }
